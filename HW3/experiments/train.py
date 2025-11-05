b77faa965e50f2c99efe79d848023ee4e949cdd1
Your Name <you@example.com>
2025-11-06 00:26:49 +0800

    fix model

--- HW3/experiments/train.py
+++ HW3/experiments/train.py
@@ -290,7 +290,7 @@ def main():
     print(f"Creating model: type={args.model_type}, layers={args.n_layer}, embd={args.n_embd}, heads={args.n_head}")
     model = create_model(
         model_type=args.model_type,
-        vocab_size=max(tokenizer.vocab.keys()),
+        vocab_size=len(tokenizer.vocab),
         max_seq_len=args.max_seq_len,
         n_layer=args.n_layer,
         n_embd=args.n_embd,
