6c76d95326b419ac5aa0a983bb4c9464433217d4
Your Name <you@example.com>
2025-11-11 10:19:20 +0800

    print all

--- HW3/experiments/inference.py
+++ HW3/experiments/inference.py
@@ -338,6 +338,8 @@ def save_cpword_tokens_as_midi(tokens, tokenizer, output_path):
         tokens = np.concatenate(tokens, axis=0)
     
     tokens = np.where(tokens < 4, 4, tokens)
+    
+    np.set_printoptions(threshold=np.inf, linewidth=200)
     print([tokens.tolist()])
     
     # Clip to valid range
