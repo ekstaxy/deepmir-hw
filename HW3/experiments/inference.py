d123ac8a52a3c8dd0e01087963b07541ca122468
Your Name <you@example.com>
2025-11-11 09:49:28 +0800

    add sys to inference.py

--- HW3/experiments/inference.py
+++ HW3/experiments/inference.py
@@ -1,7 +1,9 @@
 """
 inference.py - Music generation inference with CPWord support and continuation
 """
-
+import sys
+project_root = Path(__file__).parent.parent
+sys.path.append(str(project_root))
 import torch
 import argparse
 import os
